1e71ea615f0627c191beae69c86b5fdde3e93814
rexfa <rex_fa@163.com>
2018-01-12 16:32:59 +0800

    创建工程

---
+++ PokerLottery-CSharp/EF/MysqlContext.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Pomelo.EntityFrameworkCore.MySql;
+using Pomelo.EntityFrameworkCore;
+//using PokerLottery.EF.Domain;
+using System.Reflection;
+
+namespace PokerLottery.EF
+{
+    public class MysqlContext : DbContext
+    {
+
+        public new DbSet<TEntity> Set<TEntity>() where TEntity : BaseEntity
+        {
+            return base.Set<TEntity>();
+        }
+        public MysqlContext(DbContextOptions<MysqlContext> options) : base(options)
+        { }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            var typesToRegister = GetType().GetTypeInfo().Assembly.GetTypes()
+                .Where(x => x.GetInterfaces().Any(y => y.GetTypeInfo().IsGenericType))
+                .Where(z => { return z.GetInterfaces().Where(q => q.Name.Contains("IEntityTypeConfigura")).Count() > 0 ? true : false; })
+                .ToArray();
+            var entityMethod = typeof(ModelBuilder).GetMethods().Single(x => x.Name == "Entity" &&
+                         x.IsGenericMethod &&
+                         x.ReturnType.Name == "EntityTypeBuilder`1");
+            foreach (var mappingType in typesToRegister)
+            {
+                var genericTypeArg = mappingType.GetInterfaces().Single().GenericTypeArguments.Single();
+                var genericEntityMethod = entityMethod.MakeGenericMethod(genericTypeArg);
+                var entityBuilder = genericEntityMethod.Invoke(modelBuilder, null);
+
+                dynamic configurationInstance = Activator.CreateInstance(mappingType);
+                configurationInstance.GetType().GetMethod("Configure").Invoke(configurationInstance, new[] { entityBuilder });
+
+            }
+        }
+        protected virtual TEntity AttachEntityToContext<TEntity>(TEntity entity) where TEntity : BaseEntity, new()
+        {
+            var alreadyAttached = Set<TEntity>().Local.FirstOrDefault(x => x.Id == entity.Id);
+            if (alreadyAttached == null)
+            {
+                Set<TEntity>().Attach(entity);
+                return entity;
+            }
+            else
+            {
+                return alreadyAttached;
+            }
+        }
+    }
+}
---
+++ PokerLottery-CSharp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PokerLottery.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
