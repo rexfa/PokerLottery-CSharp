7d8aa30a023eec05b3f59d9cd0a1e53b457a9375
rexfa <rex_fa@163.com>
2018-01-22 17:20:39 +0800

    serivce 尝试

--- PokerLottery-CSharp/Services/LotteryService.cs
+++ PokerLottery-CSharp/Services/LotteryService.cs
@@ -12,16 +12,31 @@ namespace PokerLottery.Services
     {
         private readonly MysqlContext _mysqlContext;
         private readonly EfRepository<LotteryIssue> _issueRepository;
+        private readonly EfRepository<LotteryPool0> _pool0Repository;
+        private readonly EfRepository<LotteryPool1> _pool1Repository;
         private readonly IMemoryCache _memoryCache;
         public LotteryService(MysqlContext mysqlContext ,IMemoryCache memoryCache)
         {
             this._mysqlContext = mysqlContext;
             _issueRepository = new EfRepository<LotteryIssue>(this._mysqlContext);
+            _pool0Repository = new EfRepository<LotteryPool0>(this._mysqlContext);
+            _pool1Repository = new EfRepository<LotteryPool1>(this._mysqlContext);
             _memoryCache = memoryCache;
         }
         public bool ConstructNewIssue(string issueName, DateTime beginTime, DateTime stopTime)
         {
-            throw new NotImplementedException();
+            DateTime now = DateTime.Now;
+            var newIssue = new LotteryIssue()
+            {
+                IssueName = issueName,
+                BeginOn =  beginTime,
+                StopOn = stopTime,
+                OrderQuantity =0,
+                Result = "",
+                CreatedOn = now
+            };
+            _issueRepository.Insert(newIssue);
+            
         }
 
         public void LotteryDraws(LotteryIssue lotteryIssue, string LotteryParameters)
@@ -33,5 +48,18 @@ namespace PokerLottery.Services
         {
             throw new NotImplementedException();
         }
+
+        #region Private
+        private void ConstructPool(int cmd)
+        {
+            if (cmd == 0)
+            {
+                if (_pool0Repository.Table.Count() != 52 * 52 * 52 * 52)
+                {
+                }
+                
+            }
+        }
+        #endregion
     }
 }
