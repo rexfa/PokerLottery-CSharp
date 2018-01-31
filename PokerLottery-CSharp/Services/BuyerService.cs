dae54537f28f5f9d73b0c927ecdd9611b512c26f
rexfa <rex_fa@163.com>
2018-01-31 18:30:23 +0800

    继续

--- PokerLottery-CSharp/Services/BuyerService.cs
+++ PokerLottery-CSharp/Services/BuyerService.cs
@@ -6,6 +6,7 @@ using PokerLottery.EF.Domain;
 using PokerLottery.EF;
 using Microsoft.Extensions.Caching.Memory;
 using PokerLottery.Configuration;
+using PokerLottery.Models;
 
 namespace PokerLottery.Services
 {
@@ -13,12 +14,18 @@ namespace PokerLottery.Services
     {
         private readonly MysqlContext _mysqlContext;
         private readonly EfRepository<LotteryBuyer> _buyerRepository;
+        private readonly EfRepository<LotteryPool0> _pool0Repository;
+        private readonly EfRepository<LotteryPool1> _pool1Repository;
+        private readonly EfRepository<LotteryIssue> _issueRepository;
         private readonly IMemoryCache _memoryCache;
 
         public BuyerService(MysqlContext mysqlContext, IMemoryCache memoryCache)
         {
             this._mysqlContext = mysqlContext;
             _buyerRepository = new EfRepository<LotteryBuyer>(this._mysqlContext);
+            _pool0Repository = new EfRepository<LotteryPool0>(this._mysqlContext);
+            _pool1Repository = new EfRepository<LotteryPool1>(this._mysqlContext);
+            _issueRepository = new EfRepository<LotteryIssue>(this._mysqlContext);
             _memoryCache = memoryCache;
         }
         public LotteryBuyer AddBuyer(string BuyerChId, string BuyerChName, string Weixin, int ChannelId)
@@ -34,6 +41,18 @@ namespace PokerLottery.Services
             };
             _buyerRepository.Insert(newBuyer);
             return newBuyer;
+        }
+
+        public IList<LotteryTicketModel> GetCurrentLotteysByBuyer(LotteryBuyer lotteryBuyer)
+        {
+            var issue = _issueRepository.Table.OrderByDescending(i => i.CreatedOn).FirstOrDefault();
+            if (issue.Stat == (int)IssueStat.Accomplish)
+                throw new Exception("当前期没有生成。");
+            if(issue.PoolCmd==0)
+            { }
+            else if(issue.PoolCmd==1)
+            { }
+            else { throw new Exception("奖池命令错误！"); }
         }
     }
 }
